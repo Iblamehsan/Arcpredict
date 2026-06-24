ff311c8e55a5be437601dcf5324d42cf0f9d94f7
Iblamehsan <amerehehsan@gmail.com>
2026-06-24 12:28:55 +0330

    feat: add dynamic CORS middleware
    Configure CORS headers to allow cross-origin requests from external frontends like Netlify.

--- server.ts
+++ server.ts
@@ -10,6 +10,23 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
+// Dynamic CORS Middleware to support external frontends (e.g. Netlify)
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // Path to bets storage
